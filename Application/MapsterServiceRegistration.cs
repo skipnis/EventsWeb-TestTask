cbe88f17a6605b5ba93137251b75bca63e79dcc6
kipnisalex <kipnis.work@gmail.com>
2025-03-03 23:19:59 +0300

    Application Layer services registration added

---
+++ Application/MapsterServiceRegistration.cs
@@ -0,0 +1,18 @@
+using Application.Mapping;
+using Mapster;
+using MapsterMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class MapsterServiceRegistration
+{
+    public static IServiceCollection AddMapsterConfiguration(this IServiceCollection services)
+    {
+        var config = TypeAdapterConfig.GlobalSettings;
+        MapsterConfig.Configure(); 
+        services.AddSingleton(config);
+        services.AddScoped<IMapper, ServiceMapper>(); 
+        return services;
+    }
+}
