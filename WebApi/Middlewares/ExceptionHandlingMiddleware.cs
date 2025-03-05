504ca27c6b870d9a6fcd2b849c732c63a1122d15
kipnisalex <kipnis.work@gmail.com>
2025-03-05 09:50:42 +0300

    global exception handling

---
+++ WebApi/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,57 @@
+using System.Net;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace WebApi.Middlewares;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"An error occured, exception thrown by the request.");
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var response = context.Response;
+        response.ContentType = "application/json";
+        response.StatusCode = exception switch
+        {
+            ArgumentException => (int)HttpStatusCode.BadRequest,
+            UnauthorizedAccessException => (int)HttpStatusCode.Unauthorized,
+            _ => (int)HttpStatusCode.InternalServerError
+        };
+
+        var errorResponse = new
+        {
+            statusCode = response.StatusCode,
+            message = exception.Message
+        };
+
+        var options = new JsonSerializerOptions
+        {
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            WriteIndented = true 
+        };
+        
+        var result = JsonSerializer.Serialize(errorResponse, options);
+        return response.WriteAsync(result);
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,6 @@
 using Application;
 using Infrastructure;
+using WebApi.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.MapControllers();
 
 app.Run();
